69d7f9b0e73df5a38946fffb978e9648d4f6b71c
motoharu-yano <muzhedgehog@list.ru>
2023-07-31 21:50:30 +0200

    add typing. fix typos. fix formatting.

--- myopic_mces/MCES_ILP.py
+++ myopic_mces/MCES_ILP.py
@@ -7,7 +7,12 @@ Created on Mon Oct  5 17:17:41 2020
 import pulp
 import networkx as nx
 
-def MCES_ILP(G1,G2,threshold,solver,solver_options={},no_ilp_threshold=False):
+from typing import Tuple
+
+
+# noinspection PyPep8Naming
+# noinspection GrazieInspection
+def MCES_ILP(G1: nx.Graph, G2: nx.Graph, threshold, solver, solver_options=None, no_ilp_threshold=False) -> Tuple[float, int]:
     """
      Calculates the exact distance between two molecules using an ILP
 
@@ -26,7 +31,7 @@ def MCES_ILP(G1,G2,threshold,solver,solver_options={},no_ilp_threshold=False):
      no_ilp_threshold: bool
          if true, always return exact distance even if it is below the threshold (slower)
 
-     Returns:
+     Returns
      -------
      float
          Distance between the molecules
@@ -36,126 +41,127 @@ def MCES_ILP(G1,G2,threshold,solver,solver_options={},no_ilp_threshold=False):
              2 : Lower bound (If the exact distance is above the threshold)
 
     """
+    if solver_options is None:
+        solver_options = {}
 
-    ILP=pulp.LpProblem("MCES", pulp.LpMinimize)
+    ILP = pulp.LpProblem("MCES", pulp.LpMinimize)
 
-    #Variables for nodepairs
-    nodepairs=[]
+    # Variables for nodepairs
+    nodepairs = []
     for i in G1.nodes:
         for j in G2.nodes:
-            if G1.nodes[i]["atom"]==G2.nodes[j]["atom"]:
-                nodepairs.append(tuple([i,j]))
-    y=pulp.LpVariable.dicts('nodepairs', nodepairs,
-                            lowBound = 0,
-                            upBound = 1,
-                            cat = pulp.LpInteger)
-    #variables for edgepairs and weight
-    edgepairs=[]
-    w={}
+            if G1.nodes[i]["atom"] == G2.nodes[j]["atom"]:
+                nodepairs.append(tuple([i, j]))
+    y = pulp.LpVariable.dicts('nodepairs', nodepairs,
+                              lowBound=0,
+                              upBound=1,
+                              cat=pulp.LpInteger)
+
+    # variables for edgepairs and weight
+    edgepairs = []
+    w = {}
+
     for i in G1.edges:
         for j in G2.edges:
-            if (G1.nodes[i[0]]["atom"]==G2.nodes[j[0]]["atom"] and G1.nodes[i[1]]["atom"]==G2.nodes[j[1]]["atom"]) or (G1.nodes[i[1]]["atom"]==G2.nodes[j[0]]["atom"] and G1.nodes[i[0]]["atom"]==G2.nodes[j[1]]["atom"]):
-                edgepairs.append(tuple([i,j]))
-                w[tuple([i,j])]=max(G1[i[0]][i[1]]["weight"],G2[j[0]][j[1]]["weight"])-min(G1[i[0]][i[1]]["weight"],G2[j[0]][j[1]]["weight"])
+            if (G1.nodes[i[0]]["atom"] == G2.nodes[j[0]]["atom"] and G1.nodes[i[1]]["atom"] == G2.nodes[j[1]]["atom"]) or (G1.nodes[i[1]]["atom"] == G2.nodes[j[0]]["atom"] and G1.nodes[i[0]]["atom"] == G2.nodes[j[1]]["atom"]):
+                edgepairs.append(tuple([i, j]))
+                w[tuple([i, j])] = max(G1[i[0]][i[1]]["weight"], G2[j[0]][j[1]]["weight"])-min(G1[i[0]][i[1]]["weight"], G2[j[0]][j[1]]["weight"])
 
-    #variables for not mapping an edge
+    # variables for not mapping an edge
     for i in G1.edges:
-        edgepairs.append(tuple([i,-1]))
-        w[tuple([i,-1])]=G1[i[0]][i[1]]["weight"]
+        edgepairs.append(tuple([i, -1]))
+        w[tuple([i, -1])] = G1[i[0]][i[1]]["weight"]
+
     for j in G2.edges:
-        edgepairs.append(tuple([-1,j]))
-        w[tuple([-1,j])]=G2[j[0]][j[1]]["weight"]
-    c=pulp.LpVariable.dicts('edgepairs', edgepairs,
-                            lowBound = 0,
-                            upBound = 1,
-                            cat = pulp.LpInteger)
+        edgepairs.append(tuple([-1, j]))
+        w[tuple([-1, j])] = G2[j[0]][j[1]]["weight"]
 
+    c = pulp.LpVariable.dicts('edgepairs', edgepairs,
+                              lowBound=0,
+                              upBound=1,
+                              cat=pulp.LpInteger)
 
-    #objective function
-    ILP += pulp.lpSum([ w[i]*c[i] for i in edgepairs])
+    # objective function
+    ILP += pulp.lpSum([w[i]*c[i] for i in edgepairs])
 
-
-
-
-    #Every node in G1 can only be mapped to at most one in G2
+    # Every node in G1 can only be mapped to at most one in G2
     for i in G1.nodes:
-        h=[]
+        h = []
         for j in G2.nodes:
-            if G1.nodes[i]["atom"]==G2.nodes[j]["atom"]:
-                h.append(tuple([i,j]))
-        ILP+=pulp.lpSum([y[k] for k in h])<=1
+            if G1.nodes[i]["atom"] == G2.nodes[j]["atom"]:
+                h.append(tuple([i, j]))
+        ILP += pulp.lpSum([y[k] for k in h]) <= 1
 
-    #Every node in G1 can only be mapped to at most one in G1
+    # Every node in G1 can only be mapped to at most one in G1
     for i in G2.nodes:
-        h=[]
+        h = []
         for j in G1.nodes:
-            if G1.nodes[j]["atom"]==G2.nodes[i]["atom"]:
-                h.append(tuple([j,i]))
-        ILP+=pulp.lpSum([y[k] for k in h])<=1
+            if G1.nodes[j]["atom"] == G2.nodes[i]["atom"]:
+                h.append(tuple([j, i]))
+        ILP += pulp.lpSum([y[k] for k in h]) <= 1
 
-    #Every edge in G1 has to be mapped to an edge in G2 or the variable for not mapping has to be 1
+    # Every edge in G1 has to be mapped to an edge in G2 or the variable for not mapping has to be 1
     for i in G1.edges:
-        ls=[]
-        rs=[]
+        ls = []
+        # rs = []
         for j in G2.edges:
-            if (G1.nodes[i[0]]["atom"]==G2.nodes[j[0]]["atom"] and G1.nodes[i[1]]["atom"]==G2.nodes[j[1]]["atom"]) or (G1.nodes[i[1]]["atom"]==G2.nodes[j[0]]["atom"] and G1.nodes[i[0]]["atom"]==G2.nodes[j[1]]["atom"]):
-                ls.append(tuple([i,j]))
-        ILP+=pulp.lpSum([c[k] for k in ls])+c[tuple([i,-1])]==1
+            if (G1.nodes[i[0]]["atom"] == G2.nodes[j[0]]["atom"] and G1.nodes[i[1]]["atom"] == G2.nodes[j[1]]["atom"]) or (G1.nodes[i[1]]["atom"] == G2.nodes[j[0]]["atom"] and G1.nodes[i[0]]["atom"] == G2.nodes[j[1]]["atom"]):
+                ls.append(tuple([i, j]))
+        ILP += pulp.lpSum([c[k] for k in ls])+c[tuple([i, -1])] == 1
 
-    #Every edge in G2 has to be mapped to an edge in G1 or the variable for not mapping has to be 1
+    # Every edge in G2 has to be mapped to an edge in G1 or the variable for not mapping has to be 1
     for i in G2.edges:
-        ls=[]
-        rs=[]
+        ls = []
+        # rs = []
         for j in G1.edges:
-            if (G1.nodes[j[0]]["atom"]==G2.nodes[i[0]]["atom"] and G1.nodes[j[1]]["atom"]==G2.nodes[i[1]]["atom"]) or (G1.nodes[j[1]]["atom"]==G2.nodes[i[0]]["atom"] and G1.nodes[j[0]]["atom"]==G2.nodes[i[1]]["atom"]):
-                ls.append(tuple([j,i]))
-        ILP+=pulp.lpSum([c[k] for k in ls])+c[tuple([-1,i])]==1
+            if (G1.nodes[j[0]]["atom"] == G2.nodes[i[0]]["atom"] and G1.nodes[j[1]]["atom"] == G2.nodes[i[1]]["atom"]) or (G1.nodes[j[1]]["atom"] == G2.nodes[i[0]]["atom"] and G1.nodes[j[0]]["atom"] == G2.nodes[i[1]]["atom"]):
+                ls.append(tuple([j, i]))
+        ILP += pulp.lpSum([c[k] for k in ls])+c[tuple([-1, i])] == 1
 
-    #The mapping of the edges has to match the mapping of the nodes
+    # The mapping of the edges has to match the mapping of the nodes
     for i in G1.nodes:
         for j in G2.edges:
-            ls=[]
+            ls = []
             for k in G1.neighbors(i):
-                if tuple([tuple([i,k]),j]) in c:
-                    ls.append(tuple([tuple([i,k]),j]))
+                if tuple([tuple([i, k]), j]) in c:
+                    ls.append(tuple([tuple([i, k]), j]))
                 else:
-                    if  tuple([tuple([k,i]),j]) in c:
-                        ls.append(tuple([tuple([k,i]),j]))
-            rs=[]
-            if G1.nodes[i]["atom"]==G2.nodes[j[0]]["atom"]:
-                rs.append(tuple([i,j[0]]))
-            if G1.nodes[i]["atom"]==G2.nodes[j[1]]["atom"]:
-                rs.append(tuple([i,j[1]]))
-            ILP+=pulp.lpSum([c[k] for k in ls])<=pulp.lpSum([y[k] for k in rs])
-
+                    if tuple([tuple([k, i]), j]) in c:
+                        ls.append(tuple([tuple([k, i]), j]))
+            rs = []
+            if G1.nodes[i]["atom"] == G2.nodes[j[0]]["atom"]:
+                rs.append(tuple([i, j[0]]))
+            if G1.nodes[i]["atom"] == G2.nodes[j[1]]["atom"]:
+                rs.append(tuple([i, j[1]]))
+            ILP += pulp.lpSum([c[k] for k in ls]) <= pulp.lpSum([y[k] for k in rs])
 
     for i in G2.nodes:
         for j in G1.edges:
-            ls=[]
+            ls = []
             for k in G2.neighbors(i):
-                if tuple([j,tuple([i,k])]) in c:
-                    ls.append(tuple([j,tuple([i,k])]))
+                if tuple([j, tuple([i, k])]) in c:
+                    ls.append(tuple([j, tuple([i, k])]))
                 else:
-                    if tuple([j,tuple([k,i])]) in c:
-                        ls.append(tuple([j,tuple([k,i])]))
-            rs=[]
-            if G2.nodes[i]["atom"]==G1.nodes[j[0]]["atom"]:
-                rs.append(tuple([j[0],i]))
-            if G2.nodes[i]["atom"]==G1.nodes[j[1]]["atom"]:
-                rs.append(tuple([j[1],i]))
-            ILP+=pulp.lpSum([c[k] for k in ls])<=pulp.lpSum(y[k] for k in rs)
+                    if tuple([j, tuple([k, i])]) in c:
+                        ls.append(tuple([j, tuple([k, i])]))
+            rs = []
+            if G2.nodes[i]["atom"] == G1.nodes[j[0]]["atom"]:
+                rs.append(tuple([j[0], i]))
+            if G2.nodes[i]["atom"] == G1.nodes[j[1]]["atom"]:
+                rs.append(tuple([j[1], i]))
+            ILP += pulp.lpSum([c[k] for k in ls]) <= pulp.lpSum(y[k] for k in rs)
 
-    #constraint for the threshold
-    if threshold!=-1 and not no_ilp_threshold:
-        ILP +=pulp.lpSum([ w[i]*c[i] for i in edgepairs])<=threshold
+    # constraint for the threshold
+    if threshold != -1 and not no_ilp_threshold:
+        ILP += pulp.lpSum([w[i]*c[i] for i in edgepairs]) <= threshold
 
-    #solve the ILP
-    if solver=="default":
+    # solve the ILP
+    if solver == "default":
         ILP.solve()
     else:
-        sol=pulp.getSolver(solver, **solver_options)
+        sol = pulp.getSolver(solver, **solver_options)
         ILP.solve(sol)
-    if ILP.status==1:
-        return float(ILP.objective.value()),1
+    if ILP.status == 1:
+        return float(ILP.objective.value()), 1
     else:
-        return threshold,2
+        return threshold, 2
