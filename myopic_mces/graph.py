42f0a0a0cfbbb877dee71cac42e1d2dda00473ce
motoharu-yano <muzhedgehog@list.ru>
2023-07-31 21:52:40 +0200

    fix formatting. fix typos. add typing.

--- myopic_mces/graph.py
+++ myopic_mces/graph.py
@@ -7,7 +7,8 @@ Created on Mon Oct  5 17:18:56 2020
 from rdkit import Chem
 import networkx as nx
 
-def construct_graph(s):
+
+def construct_graph(s: str) -> nx.Graph:
     """ 
     Converts a SMILE into a graph
      
@@ -16,20 +17,23 @@ def construct_graph(s):
     s : str 
         Smile of the molecule
         
-    Returns:
+    Returns
     -------
     networkx.classes.graph.Graph
         Graph that represents the molecule.
         The bond types are represented as edge weights.
         The atom types are represented as atom attributes of the nodes.
     """
-    #read the smile
+    # read the smiles
     m = Chem.MolFromSmiles(s)
+
     # convert the molecule into a graph
     # The bond and atom types are converted to node/edge attributes
-    G=nx.Graph()
+    g = nx.Graph()
+
     for atom in m.GetAtoms():
-        G.add_node(atom.GetIdx(),atom=atom.GetSymbol())
+        g.add_node(atom.GetIdx(), atom=atom.GetSymbol())
     for bond in m.GetBonds():
-        G.add_edge(bond.GetBeginAtom().GetIdx(),bond.GetEndAtom().GetIdx(),weight=bond.GetBondTypeAsDouble())
-    return G
+        g.add_edge(bond.GetBeginAtom().GetIdx(), bond.GetEndAtom().GetIdx(), weight=bond.GetBondTypeAsDouble())
+
+    return g
