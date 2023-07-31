e73ad358701cabaf4f856a43577e95c5ff5ad663
motoharu-yano <muzhedgehog@list.ru>
2023-07-31 22:11:02 +0200

    refactor arg names.

--- myopic_mces/MCES_ILP.py
+++ myopic_mces/MCES_ILP.py
@@ -12,15 +12,15 @@ from typing import Tuple
 
 # noinspection PyPep8Naming
 # noinspection GrazieInspection
-def MCES_ILP(G1: nx.Graph, G2: nx.Graph, threshold, solver, solver_options=None, no_ilp_threshold=False) -> Tuple[float, int]:
+def MCES_ILP(g1: nx.Graph, g2: nx.Graph, threshold, solver, solver_options=None, no_ilp_threshold=False) -> Tuple[float, int]:
     """
      Calculates the exact distance between two molecules using an ILP
 
      Parameters
      ----------
-     G1 : networkx.classes.graph.Graph
+     g1 : networkx.classes.graph.Graph
          Graph representing the first molecule.
-     G2 : networkx.classes.graph.Graph
+     g2 : networkx.classes.graph.Graph
          Graph representing the second molecule.
      threshold : int
          Threshold for the comparison. Exact distance is only calculated if the distance is lower than the threshold.
@@ -48,9 +48,9 @@ def MCES_ILP(G1: nx.Graph, G2: nx.Graph, threshold, solver, solver_options=None,
 
     # Variables for nodepairs
     nodepairs = []
-    for i in G1.nodes:
-        for j in G2.nodes:
-            if G1.nodes[i]["atom"] == G2.nodes[j]["atom"]:
+    for i in g1.nodes:
+        for j in g2.nodes:
+            if g1.nodes[i]["atom"] == g2.nodes[j]["atom"]:
                 nodepairs.append(tuple([i, j]))
     y = pulp.LpVariable.dicts('nodepairs', nodepairs,
                               lowBound=0,
@@ -61,20 +61,20 @@ def MCES_ILP(G1: nx.Graph, G2: nx.Graph, threshold, solver, solver_options=None,
     edgepairs = []
     w = {}
 
-    for i in G1.edges:
-        for j in G2.edges:
-            if (G1.nodes[i[0]]["atom"] == G2.nodes[j[0]]["atom"] and G1.nodes[i[1]]["atom"] == G2.nodes[j[1]]["atom"]) or (G1.nodes[i[1]]["atom"] == G2.nodes[j[0]]["atom"] and G1.nodes[i[0]]["atom"] == G2.nodes[j[1]]["atom"]):
+    for i in g1.edges:
+        for j in g2.edges:
+            if (g1.nodes[i[0]]["atom"] == g2.nodes[j[0]]["atom"] and g1.nodes[i[1]]["atom"] == g2.nodes[j[1]]["atom"]) or (g1.nodes[i[1]]["atom"] == g2.nodes[j[0]]["atom"] and g1.nodes[i[0]]["atom"] == g2.nodes[j[1]]["atom"]):
                 edgepairs.append(tuple([i, j]))
-                w[tuple([i, j])] = max(G1[i[0]][i[1]]["weight"], G2[j[0]][j[1]]["weight"])-min(G1[i[0]][i[1]]["weight"], G2[j[0]][j[1]]["weight"])
+                w[tuple([i, j])] = max(g1[i[0]][i[1]]["weight"], g2[j[0]][j[1]]["weight"]) - min(g1[i[0]][i[1]]["weight"], g2[j[0]][j[1]]["weight"])
 
     # variables for not mapping an edge
-    for i in G1.edges:
+    for i in g1.edges:
         edgepairs.append(tuple([i, -1]))
-        w[tuple([i, -1])] = G1[i[0]][i[1]]["weight"]
+        w[tuple([i, -1])] = g1[i[0]][i[1]]["weight"]
 
-    for j in G2.edges:
+    for j in g2.edges:
         edgepairs.append(tuple([-1, j]))
-        w[tuple([-1, j])] = G2[j[0]][j[1]]["weight"]
+        w[tuple([-1, j])] = g2[j[0]][j[1]]["weight"]
 
     c = pulp.LpVariable.dicts('edgepairs', edgepairs,
                               lowBound=0,
@@ -85,69 +85,69 @@ def MCES_ILP(G1: nx.Graph, G2: nx.Graph, threshold, solver, solver_options=None,
     ILP += pulp.lpSum([w[i]*c[i] for i in edgepairs])
 
     # Every node in G1 can only be mapped to at most one in G2
-    for i in G1.nodes:
+    for i in g1.nodes:
         h = []
-        for j in G2.nodes:
-            if G1.nodes[i]["atom"] == G2.nodes[j]["atom"]:
+        for j in g2.nodes:
+            if g1.nodes[i]["atom"] == g2.nodes[j]["atom"]:
                 h.append(tuple([i, j]))
         ILP += pulp.lpSum([y[k] for k in h]) <= 1
 
     # Every node in G1 can only be mapped to at most one in G1
-    for i in G2.nodes:
+    for i in g2.nodes:
         h = []
-        for j in G1.nodes:
-            if G1.nodes[j]["atom"] == G2.nodes[i]["atom"]:
+        for j in g1.nodes:
+            if g1.nodes[j]["atom"] == g2.nodes[i]["atom"]:
                 h.append(tuple([j, i]))
         ILP += pulp.lpSum([y[k] for k in h]) <= 1
 
     # Every edge in G1 has to be mapped to an edge in G2 or the variable for not mapping has to be 1
-    for i in G1.edges:
+    for i in g1.edges:
         ls = []
         # rs = []
-        for j in G2.edges:
-            if (G1.nodes[i[0]]["atom"] == G2.nodes[j[0]]["atom"] and G1.nodes[i[1]]["atom"] == G2.nodes[j[1]]["atom"]) or (G1.nodes[i[1]]["atom"] == G2.nodes[j[0]]["atom"] and G1.nodes[i[0]]["atom"] == G2.nodes[j[1]]["atom"]):
+        for j in g2.edges:
+            if (g1.nodes[i[0]]["atom"] == g2.nodes[j[0]]["atom"] and g1.nodes[i[1]]["atom"] == g2.nodes[j[1]]["atom"]) or (g1.nodes[i[1]]["atom"] == g2.nodes[j[0]]["atom"] and g1.nodes[i[0]]["atom"] == g2.nodes[j[1]]["atom"]):
                 ls.append(tuple([i, j]))
         ILP += pulp.lpSum([c[k] for k in ls])+c[tuple([i, -1])] == 1
 
     # Every edge in G2 has to be mapped to an edge in G1 or the variable for not mapping has to be 1
-    for i in G2.edges:
+    for i in g2.edges:
         ls = []
         # rs = []
-        for j in G1.edges:
-            if (G1.nodes[j[0]]["atom"] == G2.nodes[i[0]]["atom"] and G1.nodes[j[1]]["atom"] == G2.nodes[i[1]]["atom"]) or (G1.nodes[j[1]]["atom"] == G2.nodes[i[0]]["atom"] and G1.nodes[j[0]]["atom"] == G2.nodes[i[1]]["atom"]):
+        for j in g1.edges:
+            if (g1.nodes[j[0]]["atom"] == g2.nodes[i[0]]["atom"] and g1.nodes[j[1]]["atom"] == g2.nodes[i[1]]["atom"]) or (g1.nodes[j[1]]["atom"] == g2.nodes[i[0]]["atom"] and g1.nodes[j[0]]["atom"] == g2.nodes[i[1]]["atom"]):
                 ls.append(tuple([j, i]))
         ILP += pulp.lpSum([c[k] for k in ls])+c[tuple([-1, i])] == 1
 
     # The mapping of the edges has to match the mapping of the nodes
-    for i in G1.nodes:
-        for j in G2.edges:
+    for i in g1.nodes:
+        for j in g2.edges:
             ls = []
-            for k in G1.neighbors(i):
+            for k in g1.neighbors(i):
                 if tuple([tuple([i, k]), j]) in c:
                     ls.append(tuple([tuple([i, k]), j]))
                 else:
                     if tuple([tuple([k, i]), j]) in c:
                         ls.append(tuple([tuple([k, i]), j]))
             rs = []
-            if G1.nodes[i]["atom"] == G2.nodes[j[0]]["atom"]:
+            if g1.nodes[i]["atom"] == g2.nodes[j[0]]["atom"]:
                 rs.append(tuple([i, j[0]]))
-            if G1.nodes[i]["atom"] == G2.nodes[j[1]]["atom"]:
+            if g1.nodes[i]["atom"] == g2.nodes[j[1]]["atom"]:
                 rs.append(tuple([i, j[1]]))
             ILP += pulp.lpSum([c[k] for k in ls]) <= pulp.lpSum([y[k] for k in rs])
 
-    for i in G2.nodes:
-        for j in G1.edges:
+    for i in g2.nodes:
+        for j in g1.edges:
             ls = []
-            for k in G2.neighbors(i):
+            for k in g2.neighbors(i):
                 if tuple([j, tuple([i, k])]) in c:
                     ls.append(tuple([j, tuple([i, k])]))
                 else:
                     if tuple([j, tuple([k, i])]) in c:
                         ls.append(tuple([j, tuple([k, i])]))
             rs = []
-            if G2.nodes[i]["atom"] == G1.nodes[j[0]]["atom"]:
+            if g2.nodes[i]["atom"] == g1.nodes[j[0]]["atom"]:
                 rs.append(tuple([j[0], i]))
-            if G2.nodes[i]["atom"] == G1.nodes[j[1]]["atom"]:
+            if g2.nodes[i]["atom"] == g1.nodes[j[1]]["atom"]:
                 rs.append(tuple([j[1], i]))
             ILP += pulp.lpSum([c[k] for k in ls]) <= pulp.lpSum(y[k] for k in rs)
 
--- myopic_mces/filter_MCES.py
+++ myopic_mces/filter_MCES.py
@@ -10,15 +10,15 @@ import networkx as nx
 from typing import Tuple
 
 
-def filter1(G1: nx.Graph, G2: nx.Graph) -> float:
+def filter1(g1: nx.Graph, g2: nx.Graph) -> float:
     """
      Finds a lower bound for the distance based on degree
 
      Parameters
      ----------
-     G1 : networkx.classes.graph.Graph
+     g1 : networkx.classes.graph.Graph
          Graph representing the first molecule.
-     G2 : networkx.classes.graph.Graph
+     g2 : networkx.classes.graph.Graph
          Graph representing the second molecule.
 
      Returns
@@ -29,22 +29,22 @@ def filter1(G1: nx.Graph, G2: nx.Graph) -> float:
     """
     # Find all occurring atom types and partition by type
     atom_types1 = []
-    for i in G1.nodes:
-        if G1.nodes[i]["atom"] not in atom_types1:
-            atom_types1.append(G1.nodes[i]["atom"])
+    for i in g1.nodes:
+        if g1.nodes[i]["atom"] not in atom_types1:
+            atom_types1.append(g1.nodes[i]["atom"])
 
     type_map1 = {}
     for i in atom_types1:
-        type_map1[i] = list(filter(lambda x: i == G1.nodes[x]["atom"], G1.nodes))
+        type_map1[i] = list(filter(lambda x: i == g1.nodes[x]["atom"], g1.nodes))
 
     atom_types2 = []
-    for i in G2.nodes:
-        if G2.nodes[i]["atom"] not in atom_types2:
-            atom_types2.append(G2.nodes[i]["atom"])
+    for i in g2.nodes:
+        if g2.nodes[i]["atom"] not in atom_types2:
+            atom_types2.append(g2.nodes[i]["atom"])
 
     type_map2 = {}
     for i in atom_types2:
-        type_map2[i] = list(filter(lambda x: i == G2.nodes[x]["atom"], G2.nodes))
+        type_map2[i] = list(filter(lambda x: i == g2.nodes[x]["atom"], g2.nodes))
 
     # calculate lower bound
     difference = 0
@@ -56,41 +56,41 @@ def filter1(G1: nx.Graph, G2: nx.Graph) -> float:
             n = min(len(type_map1[i]), len(type_map2[i]))
 
             # sort by degree
-            degreelist1 = sorted(type_map1[i], key=lambda x: sum([G1[x][j]["weight"] for j in G1.neighbors(x)]), reverse=True)
-            degreelist2 = sorted(type_map2[i], key=lambda x: sum([G2[x][j]["weight"] for j in G2.neighbors(x)]), reverse=True)
+            degreelist1 = sorted(type_map1[i], key=lambda x: sum([g1[x][j]["weight"] for j in g1.neighbors(x)]), reverse=True)
+            degreelist2 = sorted(type_map2[i], key=lambda x: sum([g2[x][j]["weight"] for j in g2.neighbors(x)]), reverse=True)
 
             # map in order of sorted lists
             for j in range(n):
-                deg1 = sum([G1[degreelist1[j]][k]["weight"] for k in G1.neighbors(degreelist1[j])])
-                deg2 = sum([G2[degreelist2[j]][k]["weight"] for k in G2.neighbors(degreelist2[j])])
+                deg1 = sum([g1[degreelist1[j]][k]["weight"] for k in g1.neighbors(degreelist1[j])])
+                deg2 = sum([g2[degreelist2[j]][k]["weight"] for k in g2.neighbors(degreelist2[j])])
                 difference += abs(deg1-deg2)
             # nodes that are not mapped
             if len(degreelist1) > n:
                 for j in range(n, len(degreelist1)):
-                    difference += sum([G1[degreelist1[j]][k]["weight"] for k in G1.neighbors(degreelist1[j])])
+                    difference += sum([g1[degreelist1[j]][k]["weight"] for k in g1.neighbors(degreelist1[j])])
             if len(degreelist2) > n:
                 for j in range(n, len(degreelist2)):
-                    difference += sum([G2[degreelist2[j]][k]["weight"] for k in G2.neighbors(degreelist2[j])])
+                    difference += sum([g2[degreelist2[j]][k]["weight"] for k in g2.neighbors(degreelist2[j])])
         # atom type only in one of the graphs
         else:
             for j in type_map1[i]:
-                difference += sum([G1[j][k]["weight"] for k in G1.neighbors(j)])
+                difference += sum([g1[j][k]["weight"] for k in g1.neighbors(j)])
     for i in atom_types2:
         if i not in atom_types1:
             for j in type_map2[i]:
-                difference += sum([G2[j][k]["weight"] for k in G2.neighbors(j)])
+                difference += sum([g2[j][k]["weight"] for k in g2.neighbors(j)])
     return difference/2
 
 
-def get_cost(G1: nx.Graph, G2: nx.Graph, i: int, j: int) -> float:
+def get_cost(g1: nx.Graph, g2: nx.Graph, i: int, j: int) -> float:
     """
      Calculates the cost for mapping node i to j based on neighborhood
 
      Parameters
      ----------
-     G1 : networkx.classes.graph.Graph
+     g1 : networkx.classes.graph.Graph
          Graph representing the first molecule.
-     G2 : networkx.classes.graph.Graph
+     g2 : networkx.classes.graph.Graph
          Graph representing the second molecule.
      i : int
          Node of G1
@@ -105,21 +105,21 @@ def get_cost(G1: nx.Graph, G2: nx.Graph, i: int, j: int) -> float:
     """
     # Find all occuring atom types in neighborhood
     atom_types1 = []
-    for k in G1.neighbors(i):
-        if G1.nodes[k]["atom"] not in atom_types1:
-            atom_types1.append(G1.nodes[k]["atom"])
+    for k in g1.neighbors(i):
+        if g1.nodes[k]["atom"] not in atom_types1:
+            atom_types1.append(g1.nodes[k]["atom"])
     type_map1 = {}
     for k in atom_types1:
-        type_map1[k] = list(filter(lambda x: k == G1.nodes[x]["atom"], G1.neighbors(i)))
+        type_map1[k] = list(filter(lambda x: k == g1.nodes[x]["atom"], g1.neighbors(i)))
 
     atom_types2 = []
-    for k in G2.neighbors(j):
-        if G2.nodes[k]["atom"] not in atom_types2:
-            atom_types2.append(G2.nodes[k]["atom"])
+    for k in g2.neighbors(j):
+        if g2.nodes[k]["atom"] not in atom_types2:
+            atom_types2.append(g2.nodes[k]["atom"])
 
     type_map2 = {}
     for k in atom_types2:
-        type_map2[k] = list(filter(lambda x: k == G2.nodes[x]["atom"], G2.neighbors(j)))
+        type_map2[k] = list(filter(lambda x: k == g2.nodes[x]["atom"], g2.neighbors(j)))
 
     # calculate cost
     difference = 0.0
@@ -129,41 +129,41 @@ def get_cost(G1: nx.Graph, G2: nx.Graph, i: int, j: int) -> float:
         if k in atom_types2:
             n = min(len(type_map1[k]), len(type_map2[k]))
             # sort by incident edges by weight
-            edgelist1 = sorted(type_map1[k], key=lambda x: G1[i][x]["weight"], reverse=True)
-            edgelist2 = sorted(type_map2[k], key=lambda x: G2[j][x]["weight"], reverse=True)
+            edgelist1 = sorted(type_map1[k], key=lambda x: g1[i][x]["weight"], reverse=True)
+            edgelist2 = sorted(type_map2[k], key=lambda x: g2[j][x]["weight"], reverse=True)
 
             # map in order of sorted lists
             for l in range(n):
-                difference += (max(G1[i][edgelist1[l]]["weight"], G2[j][edgelist2[l]]["weight"])-min(G1[i][edgelist1[l]]["weight"], G2[j][edgelist2[l]]["weight"]))/2
+                difference += (max(g1[i][edgelist1[l]]["weight"], g2[j][edgelist2[l]]["weight"]) - min(g1[i][edgelist1[l]]["weight"], g2[j][edgelist2[l]]["weight"])) / 2
 
             # cost for not mapped edges
             if len(edgelist1) > n:
                 for l in range(n, len(edgelist1)):
-                    difference += G1[i][edgelist1[l]]["weight"]/2
+                    difference += g1[i][edgelist1[l]]["weight"] / 2
 
             if len(edgelist2) > n:
                 for l in range(n, len(edgelist2)):
-                    difference += G2[j][edgelist2[l]]["weight"]/2
+                    difference += g2[j][edgelist2[l]]["weight"] / 2
         else:
             for l in type_map1[k]:
-                difference += G1[i][l]["weight"]/2
+                difference += g1[i][l]["weight"] / 2
 
     for k in atom_types2:
         if k not in atom_types1:
             for l in type_map2[k]:
-                difference += G2[j][l]["weight"]/2
+                difference += g2[j][l]["weight"] / 2
     return difference
 
 
-def filter2(G1: nx.Graph, G2: nx.Graph) -> float:
+def filter2(g1: nx.Graph, g2: nx.Graph) -> float:
     """
      Finds a lower bound for the distance based on neighborhood
 
      Parameters
      ----------
-     G1 : networkx.classes.graph.Graph
+     g1 : networkx.classes.graph.Graph
          Graph representing the first molecule.
-     G2 : networkx.classes.graph.Graph
+     g2 : networkx.classes.graph.Graph
          Graph representing the second molecule.
 
      Returns
@@ -174,14 +174,14 @@ def filter2(G1: nx.Graph, G2: nx.Graph) -> float:
     """
     # Find all occuring atom types
     atom_types1 = []
-    for i in G1.nodes:
-        if G1.nodes[i]["atom"] not in atom_types1:
-            atom_types1.append(G1.nodes[i]["atom"])
+    for i in g1.nodes:
+        if g1.nodes[i]["atom"] not in atom_types1:
+            atom_types1.append(g1.nodes[i]["atom"])
 
     atom_types2 = []
-    for i in G2.nodes:
-        if G2.nodes[i]["atom"] not in atom_types2:
-            atom_types2.append(G2.nodes[i]["atom"])
+    for i in g2.nodes:
+        if g2.nodes[i]["atom"] not in atom_types2:
+            atom_types2.append(g2.nodes[i]["atom"])
 
     atom_types = atom_types1
 
@@ -195,8 +195,8 @@ def filter2(G1: nx.Graph, G2: nx.Graph) -> float:
     # handle every atom type seperately
     for i in atom_types:
         # filter by atom type
-        nodes1 = list(filter(lambda x: i == G1.nodes[x]["atom"], G1.nodes))
-        nodes2 = list(filter(lambda x: i == G2.nodes[x]["atom"], G2.nodes))
+        nodes1 = list(filter(lambda x: i == g1.nodes[x]["atom"], g1.nodes))
+        nodes2 = list(filter(lambda x: i == g2.nodes[x]["atom"], g2.nodes))
 
         # Create new graph for and solve minimum weight full matching
         G = nx.Graph()
@@ -211,8 +211,8 @@ def filter2(G1: nx.Graph, G2: nx.Graph) -> float:
         # Add edges between all nodes of G1 and G2
         for j in nodes1:
             for k in nodes2:
-                if G1.nodes[j]["atom"] == G2.nodes[k]["atom"]:
-                    G.add_edge(tuple([1, j]), tuple([2, k]), weight=get_cost(G1, G2, j, k))
+                if g1.nodes[j]["atom"] == g2.nodes[k]["atom"]:
+                    G.add_edge(tuple([1, j]), tuple([2, k]), weight=get_cost(g1, g2, j, k))
 
         # Add nodes if one graph has more nodes of type i than the other
         if len(nodes1) < len(nodes2):
@@ -220,14 +220,14 @@ def filter2(G1: nx.Graph, G2: nx.Graph) -> float:
             for j in range(1, diff+1):
                 G.add_node(tuple([1, -j]))
                 for k in nodes2:
-                    G.add_edge(tuple([1, -j]), tuple([2, k]), weight=sum([G2[l][k]["weight"] for l in G2.neighbors(k)])/2)
+                    G.add_edge(tuple([1, -j]), tuple([2, k]), weight=sum([g2[l][k]["weight"] for l in g2.neighbors(k)]) / 2)
 
         if len(nodes2) < len(nodes1):
             diff = len(nodes1)-len(nodes2)
             for j in range(1, diff+1):
                 G.add_node(tuple([2, -j]))
                 for k in nodes1:
-                    G.add_edge(tuple([1, k]), tuple([2, -j]), weight=sum([G1[l][k]["weight"] for l in G1.neighbors(k)])/2)
+                    G.add_edge(tuple([1, k]), tuple([2, -j]), weight=sum([g1[l][k]["weight"] for l in g1.neighbors(k)]) / 2)
 
         # Solve minimum weight full matching
         h = nx.bipartite.minimum_weight_full_matching(G)
@@ -240,15 +240,15 @@ def filter2(G1: nx.Graph, G2: nx.Graph) -> float:
     return res
 
 
-def apply_filter(G1: nx.Graph, G2: nx.Graph, threshold: int, always_stronger_bound=True) -> Tuple[float, int]:
+def apply_filter(g1: nx.Graph, g2: nx.Graph, threshold: int, always_stronger_bound=True) -> Tuple[float, int]:
     """
      Finds a lower bound for the distance
 
      Parameters
      ----------
-     G1 : networkx.classes.graph.Graph
+     g1 : networkx.classes.graph.Graph
          Graph representing the first molecule.
-     G2 : networkx.classes.graph.Graph
+     g2 : networkx.classes.graph.Graph
          Graph representing the second molecule.
      threshold : int
          Threshold for the comparison. We want to find a lower bound that is higher than the threshold
@@ -264,14 +264,14 @@ def apply_filter(G1: nx.Graph, G2: nx.Graph, threshold: int, always_stronger_bou
 
     """
     if always_stronger_bound:
-        d = filter2(G1, G2)
+        d = filter2(g1, g2)
         return d, 4
     else:
         # calculate first lower bound
-        d = filter1(G1, G2)
+        d = filter1(g1, g2)
         # if below threshold calculate second lower bound
         if d <= threshold:
-            d = filter2(G1, G2)
+            d = filter2(g1, g2)
             if d <= threshold:
                 return d, 2
 
