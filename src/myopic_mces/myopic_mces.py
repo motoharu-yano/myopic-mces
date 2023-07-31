55724b981881d7010c8df7a916a4ba2f7ed2847d
motoharu-yano <muzhedgehog@list.ru>
2023-07-31 21:35:27 +0200

    cleanup typos. cleanup doc strings. cleanup code, split into smaller functions. separate MCES_core primitive. add typing.

--- src/myopic_mces/myopic_mces.py
+++ src/myopic_mces/myopic_mces.py
@@ -4,16 +4,43 @@ Created on Mon Oct  5 17:16:05 2020
 @author: seipp
 """
 import time
-import pulp
-import networkx as nx
-from joblib import Parallel, delayed
-import multiprocessing
 import argparse
-from myopic_mces.graph import construct_graph
-from myopic_mces.MCES_ILP import MCES_ILP
-from myopic_mces.filter_MCES import apply_filter
+import multiprocessing
 
-def MCES(ind, s1, s2, threshold, solver, solver_options={}, no_ilp_threshold=False, always_stronger_bound=True):
+import networkx as nx
+
+from typing import Tuple, List
+from joblib import Parallel, delayed
+
+from src.myopic_mces.graph import construct_graph
+from src.myopic_mces.MCES_ILP import MCES_ILP
+from src.myopic_mces.filter_MCES import apply_filter
+
+
+# noinspection PyPep8Naming
+def MCES_core(ind: int, g1: nx.Graph, g2: nx.Graph, threshold: int, solver: str, solver_options=None, no_ilp_threshold=False, always_stronger_bound=True) -> Tuple[int, float, int]:
+    if solver_options is None:
+        solver_options = {}
+
+    # filter out if distance is above the threshold
+    if threshold == -1:
+        res = MCES_ILP(g1, g2, threshold, solver, solver_options=solver_options, no_ilp_threshold=no_ilp_threshold)
+        return ind, res[0], res[1]
+
+    d, filter_id = apply_filter(g1, g2, threshold, always_stronger_bound=always_stronger_bound)
+
+    if d > threshold:
+        return ind, d, filter_id
+
+    # calculate MCES
+    res = MCES_ILP(g1, g2, threshold, solver, solver_options=solver_options, no_ilp_threshold=no_ilp_threshold)
+
+    return ind, res[0], res[1]
+
+
+# noinspection PyPep8Naming
+# noinspection GrazieInspection
+def MCES(ind: int, s1: str, s2: str, threshold: int, solver: str, solver_options=None, no_ilp_threshold=False, always_stronger_bound=True) -> Tuple[int, float, float, int]:
     """
     Calculates the distance between two molecules
 
@@ -27,17 +54,17 @@ def MCES(ind, s1, s2, threshold, solver, solver_options={}, no_ilp_threshold=Fal
         SMILES of the second molecule
     threshold : int
         Threshold for the comparison. Exact distance is only calculated if the distance is lower than the threshold.
-        If set to -1 the exact disatnce is always calculated.
-    solver: string
+        If set to -1 the exact distance is always calculated.
+    solver : string
         ILP-solver used for solving MCES. Example:CPLEX_CMD
-    solver_options: dict
+    solver_options : dict
         additional options to pass to solvers. Example: threads=1 for better multi-threaded performance
-    no_ilp_threshold: bool
+    no_ilp_threshold : bool
         if true, always return exact distance even if it is below the threshold (slower)
-    always_stronger_bound: bool
+    always_stronger_bound : bool
         if true, always compute and use the second stronger bound
 
-    Returns:
+    Returns
     -------
     int
         index
@@ -50,29 +77,61 @@ def MCES(ind, s1, s2, threshold, solver, solver_options={}, no_ilp_threshold=Fal
             1 : Exact Distance
             2 : Lower bound (if the exact distance is above the threshold; bound chosen dynamically)
             4 : Lower bound (second lower bound was used)
-
     """
     start = time.time()
-    # construct graph for both smiles.
-    G1 = construct_graph(s1)
-    G2 = construct_graph(s2)
-    # filter out if distance is above the threshold
-    if threshold == -1:
-        res = MCES_ILP(G1, G2, threshold, solver, solver_options=solver_options, no_ilp_threshold=no_ilp_threshold)
-        end = time.time()
-        total_time = str(end-start)
-        return ind, res[0], total_time, res[1]
-    d, filter_id = apply_filter(G1, G2, threshold, always_stronger_bound=always_stronger_bound)
-    if d > threshold:
-        end = time.time()
-        total_time = str(end-start)
-        return ind, d, total_time, filter_id
-    # calculate MCES
-    res = MCES_ILP(G1, G2, threshold, solver, solver_options=solver_options, no_ilp_threshold=no_ilp_threshold)
-    end = time.time()
-    total_time = str(end-start)
-    return ind, res[0], total_time, res[1]
 
+    if solver_options is None:
+        solver_options = {}
+
+    # construct graph for both smiles.
+    g1 = construct_graph(s1)
+    g2 = construct_graph(s2)
+
+    ind, res0, res1 = MCES_core(ind, g1, g2, threshold, solver, solver_options, no_ilp_threshold, always_stronger_bound)
+
+    end = time.time()
+    total_time = end - start
+
+    return ind, res0, total_time, res1
+
+
+def read_input_file(input_file_path: str) -> List[Tuple[int, str, str]]:
+    with open(input_file_path, 'r') as f:
+        inputs = []
+        for line in f:
+            args = line.split(',')
+            inputs.append((int(args[0]), args[1], args[2]))
+        return inputs
+
+
+def write_output_file(output_file_path: str, results: List[Tuple[int, float, float, int]]):
+    with open(output_file_path, 'w') as out:
+        for i in results:
+            out.write(f'{i[0]},{i[2]},{i[1]},{i[3]}\n')
+    pass
+
+
+def main_core(args):
+    additional_mces_options = dict(no_ilp_threshold=args.no_ilp_threshold, solver_options=dict(), always_stronger_bound=not args.choose_bound_dynamically)
+
+    if args.solver_onethreaded:
+        additional_mces_options['solver_options']['threads'] = 1
+    if args.solver_no_msg:
+        additional_mces_options['solver_options']['msg'] = False
+
+    inputs = read_input_file(args.input)
+
+    num_jobs = multiprocessing.cpu_count() if args.num_jobs is None else args.num_jobs
+    if num_jobs > 1:
+        results = Parallel(n_jobs=num_jobs, verbose=5)(delayed(MCES)(ind, s1, s2, args.threshold, args.solver, **additional_mces_options) for ind, s1, s2 in inputs)
+    else:
+        results = [MCES(ind, s1, s2, args.threshold, args.solver, **additional_mces_options) for ind, s1, s2 in inputs]
+
+    write_output_file(args.output, results)
+    pass
+
+
+# noinspection SpellCheckingInspection
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -98,36 +157,9 @@ def main():
                         "By default this is set to the number of (logical) CPU cores.")
     args = parser.parse_args()
 
-    threshold = args.threshold
+    main_core(args)
+    pass
 
-    num_jobs = multiprocessing.cpu_count() if args.num_jobs is None else args.num_jobs
-    additional_mces_options = dict(no_ilp_threshold=args.no_ilp_threshold, solver_options=dict(),
-                                   always_stronger_bound=not args.choose_bound_dynamically)
-    if args.solver_onethreaded:
-        additional_mces_options['solver_options']['threads'] = 1
-    if args.solver_no_msg:
-        additional_mces_options['solver_options']['msg'] = False
-    F = args.input
-    F2 = args.output
-    f = open(F, "r")
-    solver = args.solver
-
-    inputs = []
-    for line in f:
-        args = line.split(",")
-        inputs.append(tuple([args[0], args[1], args[2]]))
-    f.close()
-
-    if num_jobs > 1:
-        results = Parallel(n_jobs=num_jobs, verbose=5)(
-            delayed(MCES)(i[0], i[1], i[2], threshold, solver, **additional_mces_options) for i in inputs)
-    else:
-        results = [MCES(i[0], i[1], i[2], threshold, solver, **additional_mces_options) for i in inputs]
-
-    out = open(F2, "w")
-    for i in results:
-        out.write(i[0]+","+i[2]+","+str(i[1])+","+str(i[3])+"\n")
-    out.close()
 
 if __name__ == '__main__':
     main()
